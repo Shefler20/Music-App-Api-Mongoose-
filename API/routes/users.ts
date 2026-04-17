8557f7a1c91176e555e51ee181065d20597e29a6
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-04-17 16:43:05 +0500

    [Frontend.91] добавил токены refresh и access

--- API/routes/users.ts
+++ API/routes/users.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import User from "../models/User";
-import auth, {RequestWithUser} from "../middleware/auth";
 import {OAuth2Client} from "google-auth-library";
 import config from "../config";
 import {imagesUpload} from "../middleware/multer";
+import jwt from "jsonwebtoken";
 
 const usersRouter = express.Router();
+
+const createAccessToken = (userID: string) => {
+    return jwt.sign({_id: userID}, config.jwtSecret, {expiresIn: '1h'});
+};
+
+const createRefreshToken = (userID: string) => {
+    return jwt.sign({_id: userID}, config.refreshSecret, {expiresIn: '7d'});
+};
 
 usersRouter.post("/", imagesUpload.single("image"), async (req, res,next) => {
     const existingUser = await User.findOne({username: req.body.username});
@@ -24,10 +32,17 @@ usersRouter.post("/", imagesUpload.single("image"), async (req, res,next) => {
            displayName: req.body.displayName,
            avatar: req.file ? "images/" + req.file.filename : null,
        });
-       newUser.generateAuthToken();
+       newUser.token = createRefreshToken(newUser._id.toString());
         const saveUser = await newUser.save();
 
-        res.cookie("token", saveUser.token,
+        res.cookie("refreshToken", saveUser.token,
+            {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+
+        res.cookie("accessToken", createAccessToken(saveUser._id.toString()),
             {
                 httpOnly: true,
                 sameSite: "strict",
@@ -77,16 +92,23 @@ usersRouter.post("/google", async (req, res,next) => {
                 password: crypto.randomUUID()
             });
         }
-        user.generateAuthToken();
+        user.token = createRefreshToken(user._id.toString());
         const saveUser = await user.save();
 
-        res.cookie("token", saveUser.token,
+        res.cookie("refreshToken", saveUser.token,
             {
                 httpOnly: true,
                 sameSite: "strict",
                 maxAge: 7 * 24 * 60 * 60 * 1000
             });
-        res.send({message: "Successfully logged with Google", user});
+
+        res.cookie("accessToken", createAccessToken(saveUser._id.toString()),
+            {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+        res.send({message: "Successfully logged with Google", saveUser});
     }catch (e) {
         next(e);
     }
@@ -102,10 +124,17 @@ usersRouter.post("/sessions", async (req, res,next) => {
 
         if (!isMatch) return res.status(400).send({message:"Invalid password"});
 
-        user.generateAuthToken();
+        user.token = createRefreshToken(user._id.toString());
         const saveUser = await user.save();
 
-        res.cookie("token", saveUser.token,
+        res.cookie("refreshToken", saveUser.token,
+            {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+
+        res.cookie("accessToken", createAccessToken(saveUser._id.toString()),
             {
                 httpOnly: true,
                 sameSite: "strict",
@@ -117,19 +146,62 @@ usersRouter.post("/sessions", async (req, res,next) => {
     }
 });
 
-usersRouter.delete('/sessions', auth, async (req, res) => {
-    const {user} = req as RequestWithUser;
+usersRouter.delete('/sessions', async (req, res, next) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
 
-    user.token = '';
-    await user.save();
+        if (refreshToken) {
+            const user = await User.findOne({token: refreshToken});
 
-    res.clearCookie("token",
+            if (user) {
+                user.token = '';
+                await user.save();
+            }
+        }
+    }catch(e){
+        next(e);
+    }
+
+    res.clearCookie("refreshToken",
+        {
+            httpOnly: true,
+            sameSite: "strict",
+        });
+
+    res.clearCookie("accessToken",
         {
             httpOnly: true,
             sameSite: "strict",
         });
 
     res.send({message:"Session deleted"});
+});
+
+usersRouter.post('/token', async (req, res,next) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) return res.status(401).send({message: "Refresh token not provided"});
+
+        const decoded = jwt.verify(refreshToken, config.refreshSecret) as {_id: string};
+
+        const user = await User.findOne({_id: decoded._id, token: refreshToken});
+
+        if (!user) return res.status(401).send({message: "Invalid refresh token provided"});
+
+        const accessToken = createAccessToken(user._id.toString());
+
+        res.cookie("accessToken", accessToken,
+            {
+                httpOnly: true,
+                sameSite: "strict",
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            });
+
+        res.send({message:"Token updated"});
+    } catch (e) {
+        res.status(401).send({message: 'Invalid token'});
+    }
 });
 
 export default usersRouter;
